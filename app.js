049d689f7b2916b61f7f856d6f49e34520ac050f
Debdhriti Roy <vasusenvaikartan@gmail.com>
2022-07-03 05:28:45 +0530

    add start script

--- app.js
+++ app.js
@@ -136,6 +136,10 @@ app.use((err, req, res, next) => {
   res.status(status).render("error", { status, message, stack });
   //   res.send("AINTYA FAMAKKED?!!");
 });
-app.listen(3000, () => {
-  console.log("Listening on port 3000!");
+
+//environment variable port
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
 });
