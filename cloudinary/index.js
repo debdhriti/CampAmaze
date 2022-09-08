8d465cc4e2749c783c7deecb7ad285fb8e43938f
Debdhriti Roy <77495660+debdhriti@users.noreply.github.com>
2022-09-09 00:59:05 +0530

    Update index.js

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -12,7 +12,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: "YelpCamp",
+    folder: "CampAmaze",
     allowedFormats: ["jpeg", "png", "jpg"],
   },
 });
